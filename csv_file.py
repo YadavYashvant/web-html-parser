07967d3b69b4d02525825fd3a85b8b67c628f9b3
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-14 23:06:31 +0530

    implementing for csv

--- csv_file.py
+++ csv_file.py
@@ -3,3 +3,24 @@ from bs4 import BeautifulSoup as bs
 import csv
 
 url = str(input("Enter URL of website: "))
+
+req = requests.get(url)
+
+soup = bs(req.text, 'html.parser')
+
+titles = soup.find_all('div', attrs={'class', 'head'})
+titles_list = []
+
+count = 1
+for title in titles:
+    d = {}
+    d['Title Number'] = f'Title {count}'
+    d['Title Name'] = title.text
+    count +=1
+    titles_list.append(d)
+
+filename = 'titles.csv'
+with open(filename, 'w', newline='') as f:
+    w = csv.DictWriter(f, ['Title Number', 'Title Name'])
+    w.writeheader()
+    w.writerows(titles_list)
